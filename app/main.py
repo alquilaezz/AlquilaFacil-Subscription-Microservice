a9253f502677a61d49b2920d11902e78fb1bb93f
JCarbajal22 <u202121881@upc.edu.pe>
2025-12-06 10:55:18 -0500

    CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers import subscriptions, invoices, plans
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Subscriptions Service")
+
+origins = [
+    "http://localhost:5173",                # para desarrollo local
+    "https://alquilaezz.netlify.app",       # tu front en producción
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # o ["*"] si quieres permitir todos (no recomendado para prod)
+    allow_credentials=True,
+    allow_methods=["*"],            # GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],            # "Content-Type", "Authorization", etc.
+)
 
 app.include_router(plans.router)
 app.include_router(subscriptions.router)
